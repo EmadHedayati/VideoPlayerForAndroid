26418c39f72fbc9cc11cd78cea759e33b0e8c3dc
Emad Hedayati <hedayati.emad@gmail.com>
2015-09-18 09:38:00 +0430

    changing result from jsonObject to String

--- app/src/main/java/erixe/android/videoplayer/EWebServices/WebServiceRespond.java
+++ app/src/main/java/erixe/android/videoplayer/EWebServices/WebServiceRespond.java
@@ -1,20 +1,18 @@
 package erixe.android.videoplayer.EWebServices;
 
-import org.json.JSONObject;
-
 /**
  * Created by emad on 9/4/2015.
  */
 public class WebServiceRespond {
     public boolean ok;
     public String description;
-    public JSONObject result;
+    public String result;
 
     public WebServiceRespond(){
 
     }
 
-    public WebServiceRespond(boolean taskSucceeded, String message, JSONObject json)
+    public WebServiceRespond(boolean taskSucceeded, String message, String json)
     {
         this.ok = taskSucceeded;
         this.description = message;
