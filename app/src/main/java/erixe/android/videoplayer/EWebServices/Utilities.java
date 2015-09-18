869429275d4b33af4411cb924b0778b534a35326
Emad Hedayati <hedayati.emad@gmail.com>
2015-09-18 09:37:16 +0430

    fixing initializeWebServiceRespond

--- app/src/main/java/erixe/android/videoplayer/EWebServices/Utilities.java
+++ app/src/main/java/erixe/android/videoplayer/EWebServices/Utilities.java
@@ -3,6 +3,7 @@ package erixe.android.videoplayer.EWebServices;
 import android.util.Base64;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -67,7 +68,7 @@ public class Utilities {
         HttpURLConnection connection = (HttpURLConnection) httpUrl.openConnection();
 
         final String basicAuth = "Basic " + Base64.encodeToString((username + ":" + password).getBytes(), Base64.NO_WRAP);
-        connection.setRequestProperty ("Authorization", basicAuth);
+        connection.setRequestProperty("Authorization", basicAuth);
         connection.setInstanceFollowRedirects(true);
         connection.setRequestMethod(methodType.toUpperCase());
         if(methodType.toUpperCase().equals(EWebServiceStrings.METHOD_TYPE_POST))
@@ -77,11 +78,22 @@ public class Utilities {
     }
 
     public static InputStream getWebServiceInputStream(String methodType, String username, String password,
-                                                       String url, Map<String, String> paramsMap) throws IOException {
+                                                       String url, Map<String, String> paramsMap) throws Exception {
+
+        if(methodType.equals(EWebServiceStrings.METHOD_TYPE_GET) && paramsMap != null)
+            url = EWebServiceStrings.addParams(paramsMap, url);
         HttpURLConnection connection = makeHttpURLConnection(methodType, username, password, url);
-        if(paramsMap != null)
+        if(methodType.equals(EWebServiceStrings.METHOD_TYPE_POST) && paramsMap != null)
             setQueryToHttpURLConnection(connection, paramsMap);
-        InputStream inputStream = connection.getInputStream();
+
+        InputStream inputStream;
+        int responseCode = connection.getResponseCode();
+        if (responseCode >= 400)
+//            throw new Exception("Bad authentication status: " + responseCode);
+            inputStream = connection.getErrorStream();
+        else
+            inputStream = connection.getInputStream();
+
         return inputStream;
     }
 
@@ -101,7 +113,7 @@ public class Utilities {
         try {
             InputStream inputStream = Utilities.getWebServiceInputStream(methodType, username, password, url, paramsMap);
             if (inputStream != null)
-                webServiceRespond = new WebServiceRespond(true, null, new JSONObject(Utilities.convertInputStreamToString(inputStream)));
+                webServiceRespond = new WebServiceRespond(true, null, Utilities.convertInputStreamToString(inputStream));
             else
                 webServiceRespond = new WebServiceRespond(false, EWebServiceStrings.TASK_FAILED_TO_CONNECT, null);
         } catch (Exception e) {
@@ -112,23 +124,37 @@ public class Utilities {
     }
 
     public static WebServiceRespond initializeWebServiceRespond(WebServiceRespond webServiceRespond) {
-        try
-        {
-            String result = webServiceRespond.result.getString("ok");
-            String message = webServiceRespond.result.getString("description");
-            JSONObject content = webServiceRespond.result.getJSONObject("result");
+        try {
+            String ok = new JSONObject(webServiceRespond.result).getString("ok");
+            String description = new JSONObject(webServiceRespond.result).getString("description");
+            String result;
+            try {
+                JSONObject resultJson = new JSONObject(webServiceRespond.result).getJSONObject("result");
+                result = resultJson.toString();
+            } catch (JSONException e) {
+                try
+                {
+                    JSONArray resultJson = new JSONObject(webServiceRespond.result).getJSONArray("result");
+                    result = resultJson.toString();
+                }
+                catch (JSONException e2)
+                {
+                    if(e2.getMessage().toLowerCase().startsWith("value null at"))
+                        return new WebServiceRespond(Boolean.parseBoolean(ok), description, null);
+                    return new WebServiceRespond(false, e2.getMessage(), null);
+                }
+            }
 
-            if(result.equals(EWebServiceStrings.TASK_SUCCEEDED))
-                return new WebServiceRespond(true, message, content);
-            if(result.equals(EWebServiceStrings.TASK_FAILED))
-                return new WebServiceRespond(false, message, content);
+            if (ok.equals(EWebServiceStrings.TASK_SUCCEEDED))
+                return new WebServiceRespond(true, description, result);
+            if (ok.equals(EWebServiceStrings.TASK_FAILED))
+                return new WebServiceRespond(false, description, result);
         }
         catch (JSONException e)
         {
             e.printStackTrace();
             return new WebServiceRespond(false, e.getMessage(), null);
         }
-
 
         String shomare = "";
         return new WebServiceRespond(false, "", null);
