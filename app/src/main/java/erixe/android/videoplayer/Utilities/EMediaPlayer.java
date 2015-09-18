70c2872bf122cad707c1640b0662203f8b10bda8
Emad Hedayati <hedayati.emad@gmail.com>
2015-09-18 09:40:41 +0430

    adding new functions

--- app/src/main/java/erixe/android/videoplayer/Utilities/EMediaPlayer.java
+++ app/src/main/java/erixe/android/videoplayer/Utilities/EMediaPlayer.java
@@ -1,6 +1,8 @@
 package erixe.android.videoplayer.Utilities;
 
 import android.media.MediaPlayer;
+import android.media.TimedText;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -19,8 +21,9 @@ public class EMediaPlayer extends MediaPlayer implements Serializable{
     public static final int MEDIA_FINISHED = 6;
 
     public int state = 0;
-    public String mediaPath = "android.resource://com.example.emad.pagertest/raw/video";
-    OnEMediaPlayerPreparedListener callBack;
+    public String mediaPath = "http://video.erixe.com/videos/11/qualities/original.mp4";
+    public String subtitlePath = "";
+    transient OnEMediaPlayerPreparedListener callBack;
 
     public EMediaPlayer(OnEMediaPlayerPreparedListener onEMediaPlayerPreparedListener)
     {
@@ -28,12 +31,61 @@ public class EMediaPlayer extends MediaPlayer implements Serializable{
         callBack = onEMediaPlayerPreparedListener;
     }
 
-    public void setMediaPath(String path) throws IOException {
+    public void setOnEMediaPlayerPreparedListener(OnEMediaPlayerPreparedListener onEMediaPlayerPreparedListener)
+    {
+        callBack = onEMediaPlayerPreparedListener;
+    }
+
+    public void setMediaPath(String path) {
 //        this.setDataSource(path);
         this.reset();
         state = 0;
-        this.setDataSource(path);
+        try {
+            this.setDataSource(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         mediaPath = path;
+    }
+
+    public void setMediaPath(String path, String subtitlePath) {
+//        this.setDataSource(path);
+        this.reset();
+        state = 0;
+        try {
+            this.setDataSource(path);
+            this.addTimedTextSource(subtitlePath, MediaPlayer.MEDIA_MIMETYPE_TEXT_SUBRIP);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPath = path;
+        this.subtitlePath = subtitlePath;
+
+        int textTrackIndex = findTrackIndexFor(
+                MediaPlayer.TrackInfo.MEDIA_TRACK_TYPE_TIMEDTEXT, this.getTrackInfo());
+        if (textTrackIndex >= 0) {
+            this.selectTrack(textTrackIndex);
+        } else {
+            Log.w("test", "Cannot find text track!");
+        }
+
+        this.setOnTimedTextListener(new MediaPlayer.OnTimedTextListener() {
+            @Override
+            public void onTimedText(MediaPlayer mediaPlayer, TimedText timedText) {
+                if(timedText != null && callBack != null)
+                    callBack.onTimedTextReady(timedText.getText().toString());
+            }
+        });
+    }
+
+    private int findTrackIndexFor(int mediaTrackType, MediaPlayer.TrackInfo[] trackInfo) {
+        int index = -1;
+        for (int i = 0; i < trackInfo.length; i++) {
+            if (trackInfo[i].getTrackType() == mediaTrackType) {
+                return i;
+            }
+        }
+        return index;
     }
 
     public void prepareMedia() {
@@ -42,15 +94,38 @@ public class EMediaPlayer extends MediaPlayer implements Serializable{
                 @Override
                 public void onPrepared(MediaPlayer mp) {
                     state = MEDIA_PREPARED;
-                    seekTo(0);
-
                     if(callBack != null)
                         callBack.onEMediaPlayerPrepared();
                 }
             });
-            this.prepareAsync();
             state = MEDIA_PREPARING;
+            this.prepareAsync();
         }
+    }
+
+    public void changeQualityTo(String path, final int seekTo) {
+        this.reset();
+        state = 0;
+        try {
+            this.setDataSource(mediaPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPath = path;
+
+        this.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                state = MEDIA_PREPARED;
+                seekTo(seekTo);
+                EMediaPlayer.this.playMedia();
+
+                if(callBack != null)
+                    callBack.onQualityChanged();
+            }
+        });
+        state = MEDIA_PREPARING;
+        this.prepareAsync();
     }
 
     public void playMedia() {
@@ -70,6 +145,8 @@ public class EMediaPlayer extends MediaPlayer implements Serializable{
     public interface OnEMediaPlayerPreparedListener
     {
         void onEMediaPlayerPrepared();
+        void onQualityChanged();
+        void onTimedTextReady(String newSubtitleText);
     }
 
 }
